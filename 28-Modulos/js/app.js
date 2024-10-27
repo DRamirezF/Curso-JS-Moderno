d4c00bc8790a6251a6962db2b7f37161a6229aa4
Daniel Avelino <danielramirez@destiny.ws>
2024-10-27 18:56:07 -0400

    222/223: Exportar Funciones y Clases

--- 28-Modulos/js/app.js
+++ 28-Modulos/js/app.js
@@ -1,4 +1,12 @@
-import { nombreCliente, ahorro } from './cliente.js'
+import { nombreCliente, ahorro, mostrarinformacion, tieneSaldo, Cliente } from './cliente.js'
 
 console.log(nombreCliente);
 console.log(ahorro);
+
+console.log( mostrarinformacion(nombreCliente, ahorro) );
+tieneSaldo(ahorro);
+
+const cliente = new Cliente( nombreCliente, ahorro )
+
+console.log(cliente);
+console.log(cliente.mostrarinformacion());
--- 28-Modulos/js/cliente.js
+++ 28-Modulos/js/cliente.js
@@ -1,2 +1,26 @@
 export const nombreCliente = 'Juan';
 export const ahorro = 200;
+
+export function mostrarinformacion(nombre, ahorro) {
+    return `Cliente: ${nombre} - Ahorro: ${ahorro}`
+}
+
+export function tieneSaldo(ahorro) {
+    if (ahorro > 0) {
+        console.log('Sí tiene saldo');
+    } else {
+        console.log('No tiene saldo');
+    }
+}
+
+
+export class Cliente {
+    constructor(nombre, ahorro) {
+        this.nombre = nombre;
+        this.ahorro = ahorro;
+    }
+
+    mostrarinformacion() {
+        return `Cliente: ${this.nombre} - Ahorro: ${this.ahorro}`
+    }
+}
